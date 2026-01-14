2b7095f93fc3276c7a58e5ecd7dc0610c80bfb8e
Zenith-One <the.number.zero@gmail.com>
2026-01-13 23:09:12 -0500

    Fixed a typo in which 8 was replaced with B in the plaintext wheel

--- src/lib/decoder-ring.ts
+++ src/lib/decoder-ring.ts
@@ -1,4 +1,4 @@
-export const plaintextArray = 'ACT3ZKWF1XGQEOJLD9BVS57IP206UM4RHNYB'.split('');
+export const plaintextArray = 'ACT3ZKWF1XGQEOJLD9BVS57IP206UM4RHNY8'.split('');
 const ciphertextArray = [ '2', '28', '21', '34', '7', '16', '30', '5', '1', '22', '13', '10', '18', '26', '31', '24', '17', '4', '29', '12', '8', '35', '6', '23', '15', '9', '33', '3', '20', '11', '36', '27', '14', '25', '19', '32' ];
 
 type CipherPermutation = {
